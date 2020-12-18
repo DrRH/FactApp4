a1d27572757c86430e35b4b14c3556e4f10e9a9d
DrRH <64128983+DrRH@users.noreply.github.com>
2020-12-17 21:28:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ userstatus = UserStatus()
 from fastapi.middleware.cors import CORSMiddleware
 origins = [
 "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
-"http://localhost", "http://localhost:8080", "https://factapp412.herokuapp.com",
+"http://localhost", "http://localhost:8080", "https://factapp412.herokuapp.com", "https://factapp4-vista.herokuapp.com",
 ]
 app.add_middleware(
 CORSMiddleware, allow_origins=origins,
