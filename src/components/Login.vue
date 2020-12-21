e0eebe72d99d4bef5b5acf016bd4d0db53358877
Laura Sofía Valencia C <75188095+lsvalenciac@users.noreply.github.com>
2020-12-20 22:25:26 -0500

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,8 +8,11 @@
       v-model="cedula"/>
     
     <label for="">Password</label>
-    <vs-input type="password" icon-after v-model="value2" placeholder="Password"
-    v-model="password"/>
+    <vs-input type="password" icon-after v-model="password" placeholder="Password">
+        <template #icon>
+        <i class='bx bx-lock-open-alt'></i>
+        </template>
+     </vs-input>
   
     <vs-button v-on:click="loginUsuario">
        Iniciar Sesion
